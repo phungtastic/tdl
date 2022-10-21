e5082fddb19c368a573754a72f9b2a2c3151de59
Anita Phung <anita.phung@hotmail.com>
2022-10-21 15:57:05 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.roczen.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning to code to escape Roczen
         </a>
       </header>
     </div>
